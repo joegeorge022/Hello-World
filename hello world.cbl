29263500b03a7d978eb44f308e3dde1b59deae7b
Joe <127773439+joegeorge022@users.noreply.github.com>
2024-11-28 19:19:22 +0530

    Update hello world.cbl

--- hello world.cbl
+++ hello world.cbl
@@ -4,3 +4,7 @@ ENVIRONMENT DIVISION.
 PROCEDURE DIVISION.
     DISPLAY 'HELLO WORLD'.
     STOP RUN.
+
+* Common business-oriented language (COBOL) is a high-level, English-like, compiled programming language that is  developed specifically for business data processing needs.
+* COBOL is easy to learn, has a standard syntax, and is portable across platforms. It's also scalable, reliable, and has advanced file handling capabilities.
+* Learn more about cobol here: https://www.ibm.com/topics/cobol

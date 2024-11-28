2a493aeea4f4d9a433d679b8faaaaaf137b98763
Joe <127773439+joegeorge022@users.noreply.github.com>
2024-11-28 19:11:42 +0530

    Create hello world.js

---
+++ hello world.js
@@ -0,0 +1,5 @@
+console.log('Hello, World!');           // Prints the message to the browser console
+
+alert("Hello, World!");                 // Prints the message in a pop-up alert box
+
+document.write('Hello, World!');        // Prints the message directly into the HTML document
